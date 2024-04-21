ddb7d348d570bb5c939ed4d6aaf3b800a877269a
narrow-space <hasanimrantri@gmail.com>
2024-04-21 21:39:00 +0600

    last

--- client/src/api/Helper.js
+++ client/src/api/Helper.js
@@ -1,4 +1,5 @@
 
-// export const BASE_URL="https://emart-shop.vercel.app"
 
-export const BASE_URL="http://localhost:4001"
+
+// export const BASE_URL="http://localhost:4001"
+export const BASE_URL="https://emart-shop-243w.vercel.app"
